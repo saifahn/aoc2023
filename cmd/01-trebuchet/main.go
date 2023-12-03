daf15fc490e630be23e8e7ed2cd7bd22d970ad2c
Sean Li <9913851+saifahn@users.noreply.github.com>
2023-12-03 11:07:18 +0900

    wip: use map

--- cmd/01-trebuchet/main.go
+++ cmd/01-trebuchet/main.go
@@ -3,61 +3,100 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"log"
 	"os"
-	"strconv"
-	"strings"
-	"unicode"
+	"regexp"
 )
 
+var stringsToDigits = map[string]string{
+	"one":   "1",
+	"two":   "2",
+	"three": "3",
+	"four":  "4",
+	"five":  "5",
+	"six":   "6",
+	"seven": "7",
+	"eight": "8",
+	"nine":  "9",
+}
+
+var numStrings = []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+
+// create a function to check
+func getFirstAndLastNumbersFromLine(currentLine string) int {
+	matchedNumbers := map[int]string{}
+	// for each number string and char
+	// check for matches in the line
+	for _, s := range numStrings {
+		re := regexp.MustCompile(s)
+		matches := re.FindAllStringIndex(currentLine, -1)
+		if matches == nil {
+			continue
+		}
+		for _, match := range matches {
+			matchedNumbers[match[0]] = s
+		}
+	}
+	// store it in a map of map[int]string where int is the index of the number
+	// then at the end, get the keys of the map, sort them
+	// get the values of those keys from the map
+	// put them together with concatenation
+	// return the number
+	fmt.Print(matchedNumbers)
+	return 0
+}
+
 func main() {
-	file, err := os.Open("./data/01-pt1")
+	file, err := os.Open("data/01-sample")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	// create a slice to hold the list of numbers
 	numbers := []int{}
 
 	for scanner.Scan() {
-		// keep the first and last numbers in memory
-		// loop over each character
-		// if it's a number, make it first and last
-		// keep going until the end of the line
-		// put the two numbers together
-		// how best to do this?
-		// ints -> characters -> concatenation -> back to int
-		var firstNumChar, lastNumChar rune
-		firstSet := false
 		line := scanner.Text()
-		reader := bufio.NewReader(strings.NewReader(line))
-		for {
-			char, _, err := reader.ReadRune()
-			if err != nil {
-				if err == io.EOF {
-					break
-				} else {
-					log.Fatal(err)
-				}
-			}
-			if unicode.IsDigit(char) {
-				// fmt.Printf("the next char is a number: %q", char)
-				if !firstSet {
-					firstNumChar = char
-					firstSet = true
-				}
-				lastNumChar = char
-			}
-			// fmt.Printf("%q", char)
-		}
-		numberString := fmt.Sprint(string(firstNumChar), string(lastNumChar))
-		number, err := strconv.Atoi(numberString)
-		if err != nil {
-			log.Fatal(err)
-		}
+		number := getFirstAndLastNumbersFromLine(line)
+
+		// // read each character and check if it's a number
+		// for {
+		// 	char, _, err := reader.ReadRune()
+		// 	if err != nil {
+		// 		if err == io.EOF {
+		// 			break
+		// 		} else {
+		// 			log.Fatal(err)
+		// 		}
+		// 	}
+		// 	if unicode.IsDigit(char) {
+		// 		if !isFirstSet {
+		// 			firstNumChar = char
+		// 			isFirstSet = true
+		// 		}
+		// 		lastNumChar = char
+		// 		break
+		// 	}
+		// 	// naive implementation is to keep previous letter?
+		// 	// nah, we should do like lookahead
+		// 	// one
+		// 	// two
+		// 	// three
+		// 	// four
+		// 	// five
+		// 	// six
+		// 	// seven
+		// 	// eight
+		// 	// nine
+		// 	// ten
+		// }
+		// // TODO: convert word numbers to regular numbers
+		// numberString := fmt.Sprint(string(firstNumChar), string(lastNumChar))
+		// number, err := strconv.Atoi(numberString)
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
 		numbers = append(numbers, number)
 	}
 
